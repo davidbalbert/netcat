35979caf732f4c90657c7e60aae72909e21631fc
David Albert <davidbalbert@gmail.com>
2021-05-01 14:03:36 -0400

    WIP udp listener

--- main.go
+++ main.go
@@ -31,8 +31,9 @@ func lines(r io.Reader) <-chan string {
 	return out
 }
 
-func listen1(network, address string) (net.Conn, error) {
-	listener, err := net.Listen(network, address)
+// Listens on address for the first connection, and returns it
+func listen1(address string) (net.Conn, error) {
+	listener, err := net.Listen("tcp", address)
 
 	if err != nil {
 		return nil, err
@@ -47,10 +48,63 @@ func listen1(network, address string) (net.Conn, error) {
 	return conn, nil
 }
 
+type packetConnWithAddr struct {
+	net.PacketConn
+	net.Addr
+	sendBuffer [][]byte // holds messages that get sent before we have an Addr
+}
+
+func (conn *packetConnWithAddr) Read(b []byte) (n int, err error) {
+	n, addr, err := conn.ReadFrom(b)
+
+	if conn.Addr == nil {
+		conn.Addr = addr
+
+		for _, b := range conn.sendBuffer {
+			conn.WriteTo(b, addr)
+		}
+	}
+
+	return n, err
+}
+
+func (conn *packetConnWithAddr) RemoteAddr() net.Addr {
+	return conn.Addr
+}
+
+func (conn *packetConnWithAddr) Write(b []byte) (n int, err error) {
+	if conn.Addr == nil {
+		conn.sendBuffer = append(conn.sendBuffer, b)
+	}
+
+	return conn.WriteTo(b, conn.Addr)
+}
+
+func newPacketConnWithAddr(conn net.PacketConn) packetConnWithAddr {
+	sendBuffer := make([][]byte, 0, 10)
+
+	return packetConnWithAddr{conn, nil, sendBuffer}
+}
+
+// Same as listen1 but for UDP
+func listen1u(address string) (net.Conn, error) {
+	pconn, err := net.ListenPacket("udp", address)
+
+	if err != nil {
+		return nil, err
+	}
+
+	conn := newPacketConnWithAddr(pconn)
+
+	return &conn, nil
+}
+
 func main() {
 	log.SetFlags(0)
 
 	listen := flag.Bool("l", false, "Listen")
+	udp := flag.Bool("u", false, "Use UDP instead of TCP")
+
 	flag.Parse()
 
 	if flag.NArg() < 1 {
@@ -71,10 +125,17 @@ func main() {
 
 	var conn net.Conn
 	var err error
-	if *listen {
-		conn, err = listen1("tcp", address)
+	if *listen && *udp {
+		conn, err = listen1u(address)
+	} else if *listen {
+		conn, err = listen1(address)
 	} else {
-		conn, err = net.Dial("tcp", address)
+		network := "tcp"
+		if *udp {
+			network = "udp"
+		}
+
+		conn, err = net.Dial(network, address)
 	}
 
 	if err != nil {
@@ -83,6 +144,7 @@ func main() {
 
 	// TODO
 	// - add udp
+	// - handle write failure
 
 	remote := lines(conn)
 	local := lines(os.Stdin)
@@ -96,7 +158,12 @@ func main() {
 
 			fmt.Print(s)
 		case s := <-local:
-			conn.Write([]byte(s))
+			// fmt.Println(conn.RemoteAddr())
+			_, err := conn.Write([]byte(s))
+
+			if err != nil {
+				log.Fatal(err)
+			}
 		}
 	}
 }
